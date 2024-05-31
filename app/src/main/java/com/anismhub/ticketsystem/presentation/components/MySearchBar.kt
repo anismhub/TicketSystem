daddb1f31d95f4220ae3562d7a0b5d2460521b9b
anismhub <muhamadanis10@gmail.com>
2024-05-31 22:19:17 +0700

    Add "MySearchBar" Component

---
+++ app/src/main/java/com/anismhub/ticketsystem/presentation/components/MySearchBar.kt
@@ -0,0 +1,38 @@
+package com.anismhub.ticketsystem.presentation.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MySearchBar(
+    modifier: Modifier = Modifier,
+    query: String = "",
+    onQueryChange: (String) -> Unit = {},
+    onSearch: (String) -> Unit = {},
+    active: Boolean = false,
+    onActiveChange: (Boolean) -> Unit = {},
+    ) {
+    SearchBar(
+        query = query,
+        onQueryChange = onQueryChange,
+        onSearch = onSearch,
+        active = active,
+        onActiveChange = onActiveChange,
+        leadingIcon = {
+            Icon(imageVector = Icons.Rounded.Search, contentDescription = "Search Icon")
+        },
+        placeholder = {
+            Text(text = "Search")
+        },
+        modifier = modifier
+    ) { }
+}
