0167cf2ba8f3a6788459fd2eb089978f45c60bd6
Muhamad Anis <muhamadanis10@gmail.com>
2024-06-09 15:54:09 +0700

    Add DetailTicketDTO to DetailTicket

--- app/src/main/java/com/anismhub/ticketsystem/data/mapper/TicketMapper.kt
+++ app/src/main/java/com/anismhub/ticketsystem/data/mapper/TicketMapper.kt
@@ -1,8 +1,10 @@
 package com.anismhub.ticketsystem.data.mapper
 
+import com.anismhub.ticketsystem.data.remote.dto.DetailTicketDTO
 import com.anismhub.ticketsystem.data.remote.dto.ResponseDTO
 import com.anismhub.ticketsystem.data.remote.dto.TicketDTO
 import com.anismhub.ticketsystem.data.remote.dto.TicketDataDTO
+import com.anismhub.ticketsystem.domain.model.DetailTicket
 import com.anismhub.ticketsystem.domain.model.Response
 import com.anismhub.ticketsystem.domain.model.Ticket
 import com.anismhub.ticketsystem.domain.model.TicketData
@@ -25,6 +27,21 @@ fun TicketDTO.toTicket(): Ticket {
         status = status,
         message = message,
         data = data.toListTicketData()
+    )
+}
+
+fun DetailTicketDTO.toDetailTicket(): DetailTicket {
+    return DetailTicket(
+        ticketId = ticketId,
+        ticketSubject = ticketSubject,
+        ticketDescription = ticketDescription,
+        ticketPriority = ticketPriority,
+        ticketStatus = ticketStatus,
+        ticketCategory = ticketCategory,
+        ticketArea = ticketArea,
+        ticketCreatedBy = ticketCreatedBy,
+        ticketCreatedAt = ticketCreatedAt,
+        ticketUpdateAt = ticketUpdateAt
     )
 }
 
