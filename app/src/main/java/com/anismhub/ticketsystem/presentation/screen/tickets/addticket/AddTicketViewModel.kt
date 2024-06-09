6ee4db589287a4ef70c89205da4df52b33d6b940
Muhamad Anis <muhamadanis10@gmail.com>
2024-06-09 11:29:26 +0700

    Add AddTicketViewModel

---
+++ app/src/main/java/com/anismhub/ticketsystem/presentation/screen/tickets/addticket/AddTicketViewModel.kt
@@ -0,0 +1,45 @@
+package com.anismhub.ticketsystem.presentation.screen.tickets.addticket
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.anismhub.ticketsystem.domain.model.Response
+import com.anismhub.ticketsystem.domain.model.Ticket
+import com.anismhub.ticketsystem.domain.repository.TicketRepository
+import com.anismhub.ticketsystem.utils.Event
+import com.anismhub.ticketsystem.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddTicketViewModel @Inject constructor(
+    private val ticketRepository: TicketRepository
+) : ViewModel() {
+    private val _addTicket: MutableStateFlow<Event<Resource<Response>>> =
+        MutableStateFlow(Event(Resource.None))
+    val addTicket: StateFlow<Event<Resource<Response>>> = _addTicket
+
+    fun addTicket(
+        ticketSubject: String,
+        ticketDescription: String,
+        ticketPriority: Int,
+        ticketArea: Int,
+        ticketCategory: Int
+    ) {
+        viewModelScope.launch {
+            ticketRepository.addTicket(
+                ticketSubject = ticketSubject,
+                ticketDescription = ticketDescription,
+                ticketPriority = ticketPriority,
+                ticketArea = ticketArea,
+                ticketCategory = ticketCategory
+            ).collect{
+                _addTicket.value = Event(it)
+            }
+        }
+    }
+
+}
