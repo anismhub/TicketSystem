3b441f6ef5ec858421c1c0e31bbcc055d2931796
Muhamad Anis <muhamadanis10@gmail.com>
2024-06-13 21:31:50 +0700

    fix typo

--- app/src/main/java/com/anismhub/ticketsystem/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/anismhub/ticketsystem/data/repository/AuthRepositoryImpl.kt
@@ -21,7 +21,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 
-class AuthRespositoryImpl(
+class AuthRepositoryImpl(
     private val apiService: ApiService,
     private val localDataManager: LocalDataManager
 ) : AuthRepository {
@@ -82,7 +82,6 @@ class AuthRespositoryImpl(
                 emit(Resource.Error(e.message.toString()))
             }
         }
-
     }
 
     override fun getUsers(): Flow<Resource<Users>> = flow {
@@ -90,6 +89,68 @@ class AuthRespositoryImpl(
         try {
             val response = apiService.getUsers()
             emit(Resource.Success(response.toUsers()))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val jsonInString = e.response()?.errorBody()?.string()
+                val errorBody = Gson().fromJson(jsonInString, Response::class.java)
+                emit(Resource.Error(errorBody.message))
+            } else {
+                emit(Resource.Error(e.message.toString()))
+            }
+        }
+    }
+
+    override fun getUserById(userId: Int): Flow<Resource<Profile>> = flow {
+        emit(Resource.Loading)
+        try {
+            val response = apiService.getUserById(userId)
+            emit(Resource.Success(response.toProfile()))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val jsonInString = e.response()?.errorBody()?.string()
+                val errorBody = Gson().fromJson(jsonInString, Response::class.java)
+                emit(Resource.Error(errorBody.message))
+            } else {
+                emit(Resource.Error(e.message.toString()))
+            }
+        }
+    }
+
+    override fun postEditUser(
+        userId: Int,
+        username: String,
+        fullname: String,
+        role: String,
+        department: Int,
+        phoneNumber: String
+    ): Flow<Resource<Response>> = flow {
+        emit(Resource.Loading)
+        try {
+            val response = apiService.editUser(
+                userId = userId,
+                username = username,
+                fullname = fullname,
+                role = role,
+                department = department,
+                phoneNumber = phoneNumber
+            )
+            emit(Resource.Success(response.toResponse()))
+        } catch (e: Exception) {
+            if (e is HttpException) {
+                val jsonInString = e.response()?.errorBody()?.string()
+                val errorBody = Gson().fromJson(jsonInString, Response::class.java)
+                emit(Resource.Error(errorBody.message))
+            } else {
+                emit(Resource.Error(e.message.toString()))
+            }
+        }
+    }
+
+    override fun postChangePassword(userId: Int, password: String): Flow<Resource<Response>> = flow {
+        emit(Resource.Loading)
+        try {
+            val response = apiService.changePassword(userId = userId, password = password)
+            emit(Resource.Success(response.toResponse()))
         } catch (e: Exception) {
             if (e is HttpException) {
                 val jsonInString = e.response()?.errorBody()?.string()
