304ff89da232ecb8c4a757de59ca7e6c4264728e
iwan0210 <iwancuah13@gmail.com>
2024-06-17 20:13:12 +0700

    fix delete fcm token

--- app/src/main/java/com/anismhub/ticketsystem/data/manager/LocalDataManagerImpl.kt
+++ app/src/main/java/com/anismhub/ticketsystem/data/manager/LocalDataManagerImpl.kt
@@ -69,6 +69,7 @@ class LocalDataManagerImpl(context: Context) : LocalDataManager {
             pref.remove(departmentIdKey)
             pref.remove(departmentNameKey)
             pref.remove(userPhoneKey)
+            pref.remove(deviceIdKey)
         }
     }
 
--- app/src/main/java/com/anismhub/ticketsystem/data/remote/ApiService.kt
+++ app/src/main/java/com/anismhub/ticketsystem/data/remote/ApiService.kt
@@ -16,6 +16,7 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 import retrofit2.http.QueryMap
@@ -139,7 +140,7 @@ interface ApiService {
     )
 
     @FormUrlEncoded
-    @DELETE("users/token")
+    @PUT("users/token")
     suspend fun deleteToken(
         @Field("deviceId") deviceId: String
     )
