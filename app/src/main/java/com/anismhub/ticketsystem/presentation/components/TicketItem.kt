eeb08d5af2a231e2fdaeeac4e8d36ad179ab4254
Muhamad Anis <muhamadanis10@gmail.com>
2024-06-21 09:25:38 +0700

    Applying containerColor on List Ticket

--- app/src/main/java/com/anismhub/ticketsystem/presentation/components/TicketItem.kt
+++ app/src/main/java/com/anismhub/ticketsystem/presentation/components/TicketItem.kt
@@ -8,12 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.anismhub.ticketsystem.presentation.theme.LocalCustomColors
 import com.anismhub.ticketsystem.presentation.theme.MyTypography
 import com.anismhub.ticketsystem.presentation.theme.TicketSystemTheme
 
@@ -27,8 +31,24 @@ fun TicketItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val customColors = LocalCustomColors.current
     Card(
         shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = when (priority) {
+                "Tinggi" -> {
+                    MaterialTheme.colorScheme.error
+                }
+
+                "Sedang" -> {
+                    customColors.warning.colorContainer
+                }
+
+                else -> {
+                    Color.Unspecified
+                }
+            }
+        ),
         modifier = modifier
             .fillMaxWidth()
             .clickable { onClick() }
